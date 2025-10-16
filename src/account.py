bd52f534128c2ead4efc033ce6bea31ad53cc55d
Joanna Kamińska <jkaminska2@INF.UG.EDU.PL>
2025-10-16 13:57:57 +0200

    Dodanie feature 4

--- src/account.py
+++ src/account.py
@@ -1,9 +1,12 @@
 class Account:
-    def __init__(self, first_name, last_name, pesel, balance=0):
+    def __init__(self, first_name, last_name, pesel, kod=None, balance=0):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = balance
-        if len(pesel)!=11:
+        self.kod = kod
+        if isinstance(pesel,str) and len(pesel)!=11:
             self.pesel = "Invalid"
         else:
             self.pesel = pesel
+        if isinstance(kod,str) and kod.startswith("PROM_"):
+            self.balance+=50
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -7,5 +7,9 @@ class TestAccount:
         assert account.last_name == "Doe"
         assert account.balance == 0
         assert account.pesel == "93857264539"
+    def test_pesel_is_invalid(self):
         account2 = Account("Jane", "Doe", "764")
         assert account2.pesel == "Invalid"
+    def test_kod(self):
+        account2 = Account("Jane", "Doe", "764","PROM_fjds")
+        assert account2.balance==50
