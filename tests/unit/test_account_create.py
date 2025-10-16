cbd3172606f148e25ac03ec10b1f9723769388c0
Joanna Kamińska <jkaminska2@INF.UG.EDU.PL>
2025-10-16 13:28:54 +0200

    Dodanie feature 2

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -2,7 +2,8 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe","93857264539")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0
+        assert account.pesel == "93857264539"
