11f026262c28029a68f10a8e3a9405ba032a27f2
jkaminska2 <j.kaminska.251@studms.ug.edu.pl>
2025-10-18 22:38:45 +0200

    Dodanie feature 5

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -10,6 +10,15 @@ class TestAccount:
     def test_pesel_is_invalid(self):
         account2 = Account("Jane", "Doe", "764")
         assert account2.pesel == "Invalid"
-    def test_kod(self):
-        account2 = Account("Jane", "Doe", "764","PROM_fjds")
+    def test_z_kodem(self):
+        account2 = Account("Jane", "Doe", "93857264539","PROM_fjds")
         assert account2.balance==50
+    def test_bez_kodu(self):
+        account = Account("John", "Doe","93857264539")
+        assert account.balance==0
+    def test_zly_kod(self):
+        account = Account("John", "Doe","93857264539","PRM")
+        assert account.balance==0
+    def test_senior(self):
+        account2 = Account("Jane", "Doe", "50857264539", "PROM_fjds")
+        assert account2.balance == 0
