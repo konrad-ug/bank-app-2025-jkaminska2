c2fdbc14b4bd2384d613be532641bd6df88a3e0d
jkaminska2 <j.kaminska.251@studms.ug.edu.pl>
2026-01-29 14:15:51 +0100

    Feature20

--- src/repository.py
+++ src/repository.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 class AccountsRepository(ABC):
     @abstractmethod
-    def dave_all(self, account):
+    def dave_all(self, account): # pragma: no cover
         pass
     @abstractmethod
-    def load_all(self):
+    def load_all(self): # pragma: no cover
         pass
